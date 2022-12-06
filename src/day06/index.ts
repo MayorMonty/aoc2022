441e55a6d28c9e886203ab319a522f71658b7d57
Brendan McGuire <brendan@bren.app>
2022-12-06 10:32:13 -0500

    Improve code by using input slice to create sets

--- src/day06/index.ts
+++ src/day06/index.ts
@@ -6,16 +6,10 @@ const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
   for (let i = 3; i < input.length; i++) {
-
-    const a = input[i - 3];
-    const b = input[i - 2];
-    const c = input[i - 1];
-    const d = input[i];
-
-    const set = new Set([a, b, c, d]);
+    const set = new Set(input.slice(i - 3, i + 1));
 
     if (set.size === 4) {
-      return i;
+      return i + 1;
     }
 
   };
@@ -27,31 +21,11 @@ const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
   for (let i = 14; i < input.length; i++) {
-
-    const a = input[i - 14];
-    const b = input[i - 13];
-    const c = input[i - 12];
-    const d = input[i - 11];
-    const e = input[i - 10];
-    const f = input[i - 9];
-    const g = input[i - 8];
-    const h = input[i - 7];
-    const j = input[i - 6];
-    const k = input[i - 5];
-    const l = input[i - 4];
-    const m = input[i - 3];
-    const n = input[i - 2];
-    const o = input[i - 1];
-    const p = input[i];
-
-    const set = new Set([a, b, c, d, e, f, g, h, j, k, l, m, n, o, p]);
+    const set = new Set(input.slice(i - 14, i + 1));
     if (set.size === 15) {
       return i;
     }
-
   };
-
-  return;
 };
 
 run({
